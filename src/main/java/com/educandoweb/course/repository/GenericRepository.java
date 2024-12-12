d0c78c1f37bae337cbd44f147d46600fc81ee209
Igor Oliveira Silva <igoroliveirasilva772@gmail.com>
2024-12-12 18:11:55 -0300

    refactor: adjusting generics

--- src/main/java/com/educandoweb/course/repository/GenericRepository.java
+++ src/main/java/com/educandoweb/course/repository/GenericRepository.java
@@ -2,10 +2,10 @@ package com.educandoweb.course.repository;
 
 import io.micrometer.common.lang.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@NoRepositoryBean
 public interface GenericRepository<T> extends JpaRepository<T, Long> {
-
     boolean existsById(@NonNull Long id);
 }
--- src/main/java/com/educandoweb/course/service/GenericService.java
+++ src/main/java/com/educandoweb/course/service/GenericService.java
@@ -4,48 +4,92 @@ import com.educandoweb.course.domain.entity.User;
 import com.educandoweb.course.repository.GenericRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.List;
 
-import static java.util.Optional.of;
+import static com.educandoweb.course.util.Constants.ENTITY_CREATED_SUCCESS;
+import static com.educandoweb.course.util.Constants.ENTITY_DELETING;
+import static com.educandoweb.course.util.Constants.ENTITY_EXISTS;
+import static com.educandoweb.course.util.Constants.ENTITY_FINDING;
+import static com.educandoweb.course.util.Constants.ENTITY_FINDING_ALL;
+import static com.educandoweb.course.util.Constants.ENTITY_FOUND;
+import static com.educandoweb.course.util.Constants.ENTITY_NOT_FOUND_BY_ID;
+import static com.educandoweb.course.util.Constants.ENTITY_SAVING;
+import static com.educandoweb.course.util.Constants.OPERATION_SAVE_COMPLETE;
+import static com.educandoweb.course.util.Constants.OPERATION_SEARCH_COMPLETE;
 
 @Slf4j
-@Service
 @RequiredArgsConstructor
-public class GenericService<T> {
+public abstract class GenericService<T> {
 
     private final GenericRepository<T> repository;
+    protected final MessageSource messageSource;
 
-    public T save(T object) {
-        return repository.save(object);
+    protected String getMessage(String code, Object... args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 
-    public T findById(Long id) {
-        return of(repository.findById(id)).get().orElseThrow(RuntimeException::new);
+    public T save(T object, Class<T> clazz) {
+        loggingOperation(ENTITY_SAVING, clazz);
+
+        T result = repository.save(object);
+
+        loggingOperation(ENTITY_CREATED_SUCCESS, clazz);
+
+        loggingOperation(OPERATION_SAVE_COMPLETE, clazz);
+
+        return result;
     }
 
-    public List<T> findAll() {
-        return repository.findAll();
+    public T findById(Long id, Class<T> clazz) {
+        loggingOperation(ENTITY_FINDING, clazz);
+
+        T object = repository.findById(id).orElseThrow(() -> {
+            String message = getMessage(ENTITY_NOT_FOUND_BY_ID);
+            log.error(message);
+            return new RuntimeException(message);
+        });
+
+        loggingOperation(ENTITY_FOUND, clazz);
+
+        loggingOperation(OPERATION_SEARCH_COMPLETE, clazz);
+
+        return object;
     }
 
-    public T update(Long id, T object) {
-        if(!isExists(id)) {
-            return null;
-        }
+    public List<T> findAll(Class<T> clazz) {
+        loggingOperation(ENTITY_FINDING_ALL, clazz);
 
-        return repository.save(object);
+        List<T> response = repository.findAll();
+
+        loggingOperation(OPERATION_SEARCH_COMPLETE, clazz);
+
+        return response;
     }
 
-    public void delete(Long id) {
-        if (isExists(id)) {
-            repository.deleteById(id);
-        }
+    public T update(Long id, T object, Class<T> clazz) {
+        findById(id, clazz);
+
+        loggingOperation(ENTITY_SAVING, clazz);
+
+        T response = repository.save(object);
+
+        loggingOperation(OPERATION_SAVE_COMPLETE, clazz);
+
+        return response;
     }
 
-    protected boolean isExists(Long id) {
-        return repository.existsById(id);
+    public void delete(Long id, Class<T> clazz) {
+        findById(id, clazz);
+
+        loggingOperation(ENTITY_DELETING, clazz);
+
+        repository.deleteById(id);
+    }
+
+    protected void loggingOperation(String message, Class<?> clazz) {
+        log.info(getMessage(message, clazz.getSimpleName()));
     }
 }
