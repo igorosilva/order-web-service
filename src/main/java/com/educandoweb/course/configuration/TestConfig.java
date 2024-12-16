e8470c59beadfc1c0bbc64ad51724522649fd109
igorosilva <igoroliveirasilva772@gmail.com>
2024-12-16 08:15:08 -0300

    refactor: radding product and category test

--- src/main/java/com/educandoweb/course/configuration/TestConfig.java
+++ src/main/java/com/educandoweb/course/configuration/TestConfig.java
@@ -1,8 +1,12 @@
 package com.educandoweb.course.configuration;
 
+import com.educandoweb.course.domain.entity.Category;
 import com.educandoweb.course.domain.entity.Order;
+import com.educandoweb.course.domain.entity.Product;
 import com.educandoweb.course.domain.entity.User;
+import com.educandoweb.course.repository.CategoryRepository;
 import com.educandoweb.course.repository.OrderRepository;
+import com.educandoweb.course.repository.ProductRepository;
 import com.educandoweb.course.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -25,6 +29,8 @@ public class TestConfig implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final OrderRepository orderRepository;
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -35,8 +41,21 @@ public class TestConfig implements CommandLineRunner {
         Order order2 = createOrder(user2);
         Order order3 = createOrder(user1);
 
+        Product product1 = new Product(null, "Smartphone", "Latest smartphone", 599.99, "url1");
+        Product product2 = new Product(null, "Laptop", "Gaming laptop", 1199.99, "url2");
+
+        Category category1 = createCategory("Electronics");
+        Category category2 = createCategory("Books");
+        Category category3 = createCategory("Computers");
+
+        product1.getCategoryList().add(category1);
+        product2.getCategoryList().add(category1);
+        product2.getCategoryList().add(category3);
+
         userRepository.saveAll(asList(user1, user2));
         orderRepository.saveAll(asList(order1, order2, order3));
+        categoryRepository.saveAll(asList(category1, category2, category3));
+        productRepository.saveAll(asList(product1, product2));
     }
 
     private User createUser(String userName, String email, String phone) {
@@ -45,5 +64,9 @@ public class TestConfig implements CommandLineRunner {
 
     private Order createOrder(User user) {
         return new Order(null, now, WAITING_PAYMENT, List.of(), user, null);
+    }
+
+    private Category createCategory(String name) {
+        return new Category(null, name);
     }
 }
