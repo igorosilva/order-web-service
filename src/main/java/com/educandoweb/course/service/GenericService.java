27476623a5668667ffee12f5302a252abfffb190
igorosilva <igoroliveirasilva772@gmail.com>
2024-12-16 08:14:46 -0300

    refactor: renaming save to create

--- src/main/java/com/educandoweb/course/service/GenericService.java
+++ src/main/java/com/educandoweb/course/service/GenericService.java
@@ -1,6 +1,5 @@
 package com.educandoweb.course.service;
 
-import com.educandoweb.course.domain.entity.User;
 import com.educandoweb.course.repository.GenericRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,13 +9,14 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import java.util.List;
 
 import static com.educandoweb.course.util.Constants.ENTITY_CREATED_SUCCESS;
+import static com.educandoweb.course.util.Constants.ENTITY_DELETED_SUCCESS;
 import static com.educandoweb.course.util.Constants.ENTITY_DELETING;
-import static com.educandoweb.course.util.Constants.ENTITY_EXISTS;
 import static com.educandoweb.course.util.Constants.ENTITY_FINDING;
 import static com.educandoweb.course.util.Constants.ENTITY_FINDING_ALL;
 import static com.educandoweb.course.util.Constants.ENTITY_FOUND;
 import static com.educandoweb.course.util.Constants.ENTITY_NOT_FOUND_BY_ID;
 import static com.educandoweb.course.util.Constants.ENTITY_SAVING;
+import static com.educandoweb.course.util.Constants.ENTITY_UPDATED_SUCCESS;
 import static com.educandoweb.course.util.Constants.OPERATION_SAVE_COMPLETE;
 import static com.educandoweb.course.util.Constants.OPERATION_SEARCH_COMPLETE;
 
@@ -25,13 +25,14 @@ import static com.educandoweb.course.util.Constants.OPERATION_SEARCH_COMPLETE;
 public abstract class GenericService<T> {
 
     private final GenericRepository<T> repository;
+
     protected final MessageSource messageSource;
 
     protected String getMessage(String code, Object... args) {
         return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 
-    public T save(T object, Class<T> clazz) {
+    public T create(T object, Class<T> clazz) {
         loggingOperation(ENTITY_SAVING, clazz);
 
         T result = repository.save(object);
@@ -78,6 +79,8 @@ public abstract class GenericService<T> {
 
         loggingOperation(OPERATION_SAVE_COMPLETE, clazz);
 
+        loggingOperation(ENTITY_UPDATED_SUCCESS, clazz);
+
         return response;
     }
 
@@ -87,9 +90,15 @@ public abstract class GenericService<T> {
         loggingOperation(ENTITY_DELETING, clazz);
 
         repository.deleteById(id);
+
+        loggingOperation(ENTITY_DELETED_SUCCESS, clazz);
     }
 
     protected void loggingOperation(String message, Class<?> clazz) {
         log.info(getMessage(message, clazz.getSimpleName()));
+    }
+
+    protected void loggingError(String message, Class<?> clazz) {
+        log.error(getMessage(message, clazz.getSimpleName()));
     }
 }
