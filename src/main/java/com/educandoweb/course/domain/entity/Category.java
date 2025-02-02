779507a34f7f5eee0df3a10d794b535e46d400c0
igorosilva <igoroliveirasilva772@gmail.com>
2025-02-02 12:15:07 -0300

    refactor: removing product list and constructor

--- src/main/java/com/educandoweb/course/domain/entity/Category.java
+++ src/main/java/com/educandoweb/course/domain/entity/Category.java
@@ -5,18 +5,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 import static com.educandoweb.course.util.Constants.TB_CATEGORY;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
@@ -36,20 +34,11 @@ public class Category implements Serializable {
     private static final long serialVersionUID = 3761016518219785510L;
 
     @Id
+    @Include
     @JsonProperty("code")
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     @JsonProperty("name")
-    private String dsName;
-
-    @JsonProperty("products")
-    @ManyToMany(mappedBy = "categoryList")
-    private Set<Product> productList = new HashSet<>();
-
-    public Category(Long id, String dsName) {
-        super();
-        this.id = id;
-        this.dsName = dsName;
-    }
+    private String nmCategory;
 }
