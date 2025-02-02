ec74236377bd2535a8e5ed6dc2bc2f4238b292a1
igorosilva <igoroliveirasilva772@gmail.com>
2025-02-02 12:21:51 -0300

    refactor: adding custom constructor and changing List to Set type

--- src/main/java/com/educandoweb/course/domain/entity/Order.java
+++ src/main/java/com/educandoweb/course/domain/entity/Order.java
@@ -2,13 +2,11 @@ package com.educandoweb.course.domain.entity;
 
 import com.educandoweb.course.domain.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -16,24 +14,25 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.educandoweb.course.util.Constants.TB_ORDER;
 import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
-import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @FieldNameConstants
 @AllArgsConstructor
 @JsonInclude(NON_NULL)
@@ -43,27 +42,21 @@ public class Order implements Serializable {
     private static final long serialVersionUID = -8902304584597040749L;
 
     @Id
+    @Include
     @JsonProperty("code")
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     @JsonProperty("orderedAt")
-    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime _orderedAt;
 
     @JsonProperty("status")
     private OrderStatus orderStatus;
 
-    @JsonIgnore
     @JsonProperty("items")
-    @OneToMany(mappedBy = "id.order", cascade = ALL, orphanRemoval = true)
-    private List<OrderItem> productList;
-
-    @ManyToOne
-    @JsonIgnore
-    @JsonProperty("client")
-    @JoinColumn(name = "client_id")
-    private User user;
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> productList = new HashSet<>();
 
     @ManyToOne
     private Payment payment;
@@ -73,5 +66,12 @@ public class Order implements Serializable {
                 .stream()
                 .mapToDouble(OrderItem::subTotal)
                 .sum();
+    }
+
+    public Order(Long id, LocalDateTime _orderedAt, OrderStatus orderStatus, Payment payment) {
+        this.id = id;
+        this._orderedAt = _orderedAt;
+        this.orderStatus = orderStatus;
+        this.payment = payment;
     }
 }
